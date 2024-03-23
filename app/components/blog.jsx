01fec97c4016c87cba53cfd20cd1cfd83cd77e7c
Vusal Alakbarov <vusal11010@gmail.com>
2024-03-23 17:02:02 +0400

    Made first BlogCard initially hovered.

--- app/components/blog.jsx
+++ app/components/blog.jsx
@@ -1,17 +1,40 @@
+"use client";
+
 import Heading from "./heading";
 import BlogCard from "./blog-card";
+import { useState } from "react";
+import clsx from "clsx";
+
+const imageURLs = [
+  "/photos/blog-photo-1.jpg",
+  "/photos/blog-photo-2.jpg",
+  "/photos/blog-photo-3.jpg",
+  "/photos/blog-photo-4.jpg",
+];
 
 export default function Blog({ className }) {
+  const [hoveredCard, setHoveredCard] = useState(0);
+
+  function handleMouseEnter(index) {
+    setHoveredCard(index);
+  };
+
   return (
     <div
       className={`flex flex-col justify-center items-center px-8 md:px-12 lg:px-24 ${className}`}
     >
       <Heading className="mb-10">Blog</Heading>
       <div className="flex flex-col lg:flex-row justify-center items-center gap-x-1 gap-y-4">
-        <BlogCard imageURL="/photos/blog-photo-1.jpg" />
-        <BlogCard imageURL="/photos/blog-photo-2.jpg" />
-        <BlogCard imageURL="/photos/blog-photo-3.jpg" />
-        <BlogCard imageURL="/photos/blog-photo-4.jpg" />
+        {imageURLs.map((imageURL, index) => (
+          <BlogCard
+            key={index}
+            imageURL={imageURL}
+            onMouseEnter={() => handleMouseEnter(index)}
+            innerStyle={clsx({
+              "opacity-100 md:opacity-0": hoveredCard === index
+            })}
+          />
+        ))}
       </div>
     </div>
   );
